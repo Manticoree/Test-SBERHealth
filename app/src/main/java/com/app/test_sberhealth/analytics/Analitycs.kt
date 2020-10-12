128a38f2afa638407f0e9e511f15bc112720821b
Manticoree <alex.agafonov1@yandex.ru>
2020-10-12 20:17:14 +0300

    refactoring

---
+++ app/src/main/java/com/app/test_sberhealth/analytics/Analitycs.kt
@@ -0,0 +1,17 @@
+package com.app.test_sberhealth.analytics
+
+import android.util.Log
+
+class Analitycs {
+
+    companion object {
+        fun showAdultList() {
+            Log.i("Show list", "adult")
+        }
+
+        fun showChildList() {
+            Log.i("Show list", "adult")
+        }
+    }
+
+}
